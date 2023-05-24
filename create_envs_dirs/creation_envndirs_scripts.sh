475c952a050d9ca468c40e426240c3669946f84d
Bill Morrisson <billmorrissonjr@gmail.com>
2023-05-24 16:48:33 -0400

    Finishing up and clean up

--- create_envs_dirs/creation_envndirs_scripts.sh
+++ create_envs_dirs/creation_envndirs_scripts.sh
@@ -4,6 +4,8 @@
 # 0. If not already in virtualenv:
 # 0.1. If virtualenv already exists activate it,
 # 0.2. If not create it with global packages, update pip then activate it
+# 0.0.1. If application structure type is mlapp create directory structures and install appropriate packages
+# 0.0.2. ElseIf application structure type is webapp create directory structures and install appropriate packages
 # 1. If already in virtualenv: just give info
 #
 # Usage:
@@ -14,7 +16,7 @@
 # or for a specific python version and environment name;
 # $ ve python3.9 ./.venv-diff
 crenvndirs() {
-    local py=${1:-python3.8}
+    local py=${1:-python3}
     local venv="${2:-./.venv}" # Creating venv as a private directory is cool so as not to clutter the directory!
     local app_type="${3:-none}"
 
@@ -28,26 +30,33 @@ crenvndirs() {
             ${py} -m venv ${venv} --system-site-package
             #echo "export PYTHON=${py}" >> ${bin}    # overwrite ${python} on .zshenv
             source ${bin}
-            echo "Upgrading pip3"
-            ${py} -m pip3 install --upgrade pip3
+            echo "Upgrading pip"
+            ${py} -m pip install --upgrade pip
 
             # Checking for the application type, creating and installing the appriopriate directories and packages successively
-            if ["${app_type}" == "mlapp"]; then
+            if [ "${app_type}" = "mlapp" ]; then
                 echo "Creating directories and installing machine learning packages..."
+                
                 mkdir data src models notebooks
                 touch data/train.csv data/test.csv
                 touch src/train.py src/inference.py src/models.py src/config.py src/data_pipeline.py
                 touch notebooks/exploration.ipynb notebooks/check_data.ipynb
 
-                ${py} -m pip3 install numpy pandas scikit-learn torch torchvision torchaudio tensorflow
-            elif ["${app_type}" == "webapp"]; then
+                ${py} -m pip install numpy pandas scikit-learn torch torchvision torchaudio tensorflow
+
+                echo "Finished setting up machine learning environment and basic packages"
+            elif ["${app_type}" = "webapp"]; then
                 echo "Creating directories and installing web development packages"
+                
                 mkdir backend frontend backend/src backend/sql backend/tests frontend/tests
                 touch backend/__init__.py backend/admin.py backend/app.py backend/config.py backend/controllers.py backend/data_model.py backend/manage.py backend/requirements.txt
-                ${py} -m pip3 install flask django
+                ${py} -m pip install flask django
                 cd frontend/
                 npm install react react-dom --save
                 cd ..
+                
+                echo "Finished setting up web development environment and basic packages"
+            fi
 
         else
             echo "Virtual environment  ${venv} already exists, activating..."
